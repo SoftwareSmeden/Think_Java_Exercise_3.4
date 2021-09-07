fb9e61826d5757e3dfe0277b6d8b4416fd3d371c
SoftwareSmeden <torb815g@edu.easj.dk>
2021-09-07 21:26:07 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Import statements used.
+        Scanner userInput = new Scanner(System.in);
+        Random random = new Random();
+
+        //Variables named
+        int pickAINumber;
+        int pickANumber;
+
+        //Program waiting for an input.
+        System.out.println("- I'm think of a number between 1-100, \n- Can you guess what it is?: \n- Type a number: ");
+        pickANumber = userInput.nextInt();
+
+        //Output generated.
+        int randomNumberGenerator = random.nextInt(100) + 1;
+        System.out.println("- The number the computer picked: " + randomNumberGenerator);
+
+        //Second output when both numbers are picked.
+        int difference = (randomNumberGenerator - pickANumber);
+        System.out.println("- You were off by: " + difference);
+
+    }
+}
